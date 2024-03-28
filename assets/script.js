82c1da0c92ea93413b26e1192c940abf6e74f851
Dhileepguru <dhileepguru497@gmail.com>
2024-03-28 16:08:15 +0530

    Updated text to speech

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var speech= new SpeechSynthesisUtterance();
+
+function speak(){
+    speech.text= textToSpeech.value;
+    speech.volume=1;
+    speech.rate=1;
+    speech.pitch=1;
+    window.speechSynthesis.speak(speech);
+}
